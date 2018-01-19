db27969c5a4921e5900a3bb190f0752fb847fb1a
electricface <songwentai@deepin.com>
2018-01-19 18:41:00 +0800

    add script try-error

---
+++ try-error-all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ex
+cmd=./try-error.sh
+$cmd glib-2.0
+$cmd gobject-2.0
+$cmd gio-2.0
+$cmd gudev-1.0
+$cmd flatpak-1.0
+$cmd gdkpixbuf-2.0
+$cmd pango-1.0
+$cmd cairo-1.0
+$cmd atk-1.0
+$cmd gdk-3.0
+$cmd gtk-3.0
+
---
+++ try-error.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./try-error $GOPATH/src/github.com/linuxdeepin/go-gir/$1
+
